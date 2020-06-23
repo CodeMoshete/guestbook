83766fa1fba76248327a650be123fe0995baa1c6
Ben Mosher <codemoshete@gmail.com>
2020-06-23 00:13:49 -0700

    Wire up web functionality

--- GuestbookVisualizer/Assets/Source/Engine.cs
+++ GuestbookVisualizer/Assets/Source/Engine.cs
@@ -1,18 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+
+[System.Serializable]
+public class EntryResponse
+{
+    public string name;
+    public string message;
+}
 
 public class Engine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private const float QUERY_TIME = 1f;
+    private const float QUERY_CHANCE = 0.125f;
+
+    public GameObject FireworkEffect;
+    public List<MessageContainer> MessageContainers;
+    private float queryTime;
+
+    private void Start()
     {
-        
+        queryTime = QUERY_TIME;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        queryTime -= Time.deltaTime;
+        if (queryTime <= 0f)
+        {
+            queryTime = QUERY_TIME;
+            if (Random.Range(0f, 1f) <= QUERY_CHANCE)
+            {
+                StartCoroutine(RequestEntry());
+            }
+        }
+    }
+
+    private IEnumerator RequestEntry()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("http://18.191.101.1:8080/guestbook/getRandomGuestbookMessage");
+        yield return www.SendWebRequest();
+        EntryResponse response = JsonUtility.FromJson<EntryResponse>(www.downloadHandler.text);
+        ShowFireworkMessage(response.name, response.message);
+    }
+
+    private void ShowFireworkMessage(string name, string message)
+    {
+        for (int i = 0, count = MessageContainers.Count; i < count; ++i)
+        {
+            MessageContainer container = MessageContainers[i];
+            if (container.CurrentState == FadeState.Hidden)
+            {
+                container.ShowMessage(name, message);
+                GameObject.Instantiate(FireworkEffect);
+                break;
+            }
+        }
     }
 }
---
+++ GuestbookVisualizer/Assets/Source/MessageContainer.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public enum FadeState
+{
+    Hidden,
+    ShowDelay,
+    FadeIn,
+    Displaying,
+    FadeOut
+}
+
+public class MessageContainer : MonoBehaviour
+{
+    private const float DELAY_TIME = 2f;
+    private const float FADE_TIME = 1f;
+    private const float DISPLAY_TIME = 5f;
+
+    public Text NameField;
+    public Text MessageField;
+    public CanvasGroup Canvas;
+    public FadeState CurrentState { get; private set; }
+    private float displayTime;
+
+    public void ShowMessage(string name, string message)
+    {
+        if (CurrentState == FadeState.Hidden)
+        {
+            CurrentState = FadeState.ShowDelay;
+            NameField.text = name;
+            MessageField.text = message;
+            displayTime = DELAY_TIME;
+        }
+    }
+
+    private void Update()
+    {
+        displayTime -= Time.deltaTime;
+        float pct = 0f;
+
+        switch(CurrentState)
+        {
+            case FadeState.ShowDelay:
+                if (displayTime <= 0)
+                {
+                    CurrentState = FadeState.FadeIn;
+                    displayTime = FADE_TIME;
+                }
+                break;
+            case FadeState.FadeIn:
+                pct = Mathf.Min(1f - (displayTime / FADE_TIME), 1f);
+                Canvas.alpha = pct;
+                if (displayTime <= 0)
+                {
+                    CurrentState = FadeState.Displaying;
+                    displayTime = DISPLAY_TIME;
+                }
+                break;
+            case FadeState.Displaying:
+                if (displayTime <= 0)
+                {
+                    CurrentState = FadeState.FadeOut;
+                    displayTime = FADE_TIME;
+                }
+                break;
+            case FadeState.FadeOut:
+                pct = Mathf.Max(displayTime / FADE_TIME, 0f);
+                Canvas.alpha = pct;
+                if (displayTime <= 0)
+                {
+                    CurrentState = FadeState.Hidden;
+                }
+                break;
+        }
+    }
+}
