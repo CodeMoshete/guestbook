75b1344131409985e27acea870ab3722f4390536
CodeMoshete <codemoshete@gmail.com>
2020-06-24 01:21:32 -0700

    Add initial code for spawning meteors

---
+++ GuestbookVisualizer/Assets/Source/Meteor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Meteor : MonoBehaviour
+{
+    private Vector3 velocity;
+
+    public void Initialize(Vector3 vel)
+    {
+        velocity = vel;
+    }
+
+    private void Update()
+    {
+        Vector3 delta = velocity * Time.deltaTime;
+        transform.Translate(delta);
+    }
+}
---
+++ GuestbookVisualizer/Assets/Source/MeteorSpawner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class MeteorSpawner : MonoBehaviour
+{
+    private const float RANDOM_SPAWN_RATE_SECONDS = 20f;
+
+    public GameObject MeteorPrefab;
+    public Vector3 MinVelocity;
+    public Vector3 MaxVelocity;
+
+    private BoxCollider boxCollider;
+
+    private void Start()
+    {
+        boxCollider = gameObject.GetComponent<BoxCollider>();
+    }
+
+    private void Update()
+    {
+        float showChance = Time.deltaTime / RANDOM_SPAWN_RATE_SECONDS;
+        bool spawnMeteor = Random.Range(0, 1) <= showChance;
+        if (spawnMeteor)
+        {
+            float xVal = Random.Range(MinVelocity.x, MaxVelocity.x);
+            float yVal = Random.Range(MinVelocity.y, MaxVelocity.y);
+            float zVal = Random.Range(MinVelocity.z, MaxVelocity.z);
+            Vector3 velocity = new Vector3(xVal, yVal, zVal);
+            GameObject meteor = GameObject.Instantiate(MeteorPrefab);
+            meteor.GetComponent<Meteor>().Initialize(velocity);
+
+            xVal = Random.Range(boxCollider.bounds.max.x, boxCollider.bounds.min.x);
+            yVal = Random.Range(boxCollider.bounds.max.y, boxCollider.bounds.min.y);
+            zVal = Random.Range(boxCollider.bounds.max.z, boxCollider.bounds.min.z);
+            Vector3 spawnPos = new Vector3(xVal, yVal, zVal) + transform.position;
+            meteor.transform.position = spawnPos;
+        }
+    }
+}
